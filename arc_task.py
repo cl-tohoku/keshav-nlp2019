9591541400debcff872226f304f28984b74b5baf
Naoya Inoue@Whiskey <naoya-i@ecei.tohoku.ac.jp>
2019-01-12 17:28:30 +0900

    Softmax => Sigmoid, 46 => max_tokens.

--- arc_task.py
+++ arc_task.py
@@ -10,15 +10,26 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Concatenate,Activation,Embedding, Flatten, Dense, LSTM, Multiply, Input, Dot
 from keras.models import Sequential, Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+import keras
+import tensorflow as tf
 import itertools
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# To reduce memory consumption    
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+keras.backend.set_session(sess)
+    
 embeddings_index = {}
 premise = []
 claim = []
 warrant = []
 
 #Word embeddings extraction
-with open('/Users/keshavsingh/Desktop/glove.6B.100d.txt','r') as f:
+with open('/home/keshavsingh/glove.6B.100d.txt','r') as f:
     for line in f:
         values = line.split()
         word = values[0]
@@ -27,7 +38,7 @@ with open('/Users/keshavsingh/Desktop/glove.6B.100d.txt','r') as f:
 f.close()
 
 #Premise,claim extraction
-with open('/Users/keshavsingh/Desktop/train.tsv','r') as f:
+with open('/home/keshavsingh/train.tsv','r') as f:
     for line in csv.reader(f, delimiter='\t'):
         t_premise = line[4]
         t_claim = line[5]
@@ -35,20 +46,25 @@ with open('/Users/keshavsingh/Desktop/train.tsv','r') as f:
         claim.append(t_claim)
 
 #Generating data (positive and negative)
-pos_dataset = list(zip(premise,claim))
+pos_dataset = list(zip(premise, claim))
 neg_dataset = []
-for prem,clam in pos_dataset:
+for prem, claim in pos_dataset:
     premise_pool = []
-    for p,c in pos_dataset:
-        if c != clam:
+    
+    for p, c in pos_dataset:
+        if c != claim:
             premise_pool += [p]
-    neg_claim = clam
+            
+    neg_claim = claim
     neg_premise = random.sample(premise_pool, 1)[0]
     neg_dataset += [(neg_premise, neg_claim)]
 
 #Total dataset
 total_dataset = pos_dataset + neg_dataset
-label = [1]*1210 +[0]*1210
+labels = [1]*len(pos_dataset) + [0]*len(neg_dataset)
+
+logging.info("# instances: {}".format(len(total_dataset)))
+
 
 #total (premise and claim)
 total_p = []
@@ -69,8 +85,15 @@ t.fit_on_texts(vocab)
 vocab_size = len(t.word_index) +1
 encoded_premise = t.texts_to_sequences(total_p)
 encoded_claim = t.texts_to_sequences(total_c)
-padded_premise = pad_sequences(encoded_premise, maxlen=46, padding='post')
-padded_claim = pad_sequences(encoded_claim, maxlen=46, padding='post')
+
+sent_numbers = [len(s) for s in encoded_premise] + [len(s) for s in encoded_claim]
+max_tokens = max(sent_numbers)
+
+logging.info("max tokens: {}".format(max_tokens))
+
+
+padded_premise = pad_sequences(encoded_premise, maxlen=max_tokens, padding='post')
+padded_claim = pad_sequences(encoded_claim, maxlen=max_tokens, padding='post')
 
 #embedding matrix 
 embedding_matrix = zeros((vocab_size, 100))
@@ -80,19 +103,22 @@ for word,i in t.word_index.items():
         embedding_matrix[i] = embedding_vector
 
 #Keras Implementation
-input_premise = Input(shape = (46,), dtype='int32')
-input_claim = Input(shape = (46,), dtype='int32')
-emb = Embedding(output_dim = 100, input_dim = vocab_size, weights=[embedding_matrix], input_length = 46)
+input_premise = Input(shape = (max_tokens,), dtype='int32')
+input_claim = Input(shape = (max_tokens,), dtype='int32')
+emb = Embedding(output_dim = 100, input_dim = vocab_size, weights=[embedding_matrix], input_length = max_tokens)
 
 x_premise = emb(input_premise)
 x_claim = emb(input_claim)
 lstm_out_premise = LSTM(32)(x_premise)
 lstm_out_claim = LSTM(32)(x_claim)
 y = Concatenate()([lstm_out_premise, lstm_out_claim])
-output = Dense(1, activation='softmax')(y)
+output = Dense(1, activation='sigmoid')(y)
 model = Model(inputs=[input_premise, input_claim], outputs=[output])
 print(model.summary())
 #plot_model(model, to_file='claim_premise.png')
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit([padded_premise, padded_claim], label, epochs = 80,shuffle = True, batch_size = 64, verbose = 1)
+
+logging.info("Start training...")
+
+model.fit([padded_premise, padded_claim], labels, epochs = 80,shuffle = True, batch_size = 64, verbose = 1)
 
